81c8d54fffe2e673bf5fae735e0c7ba5a66fd18e
badCompany55 <zachirvin45@gmail.com>
2019-03-09 11:33:44 -0600

    ROUTER add project router file add initial setup of router file add use of router in server file

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const logger = require("morgan");
 const helm = require("helmet");
 
 const server = express();
+const projectRouter = require("./routes/projects.js");
+
+server.use(express.json());
+server.use("/projects", projectRouter);
 
 server.get("/", (req, res) => {
   res.send(`
